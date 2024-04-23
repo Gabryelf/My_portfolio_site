2b79168d7b82f48c6101e63790465552ca0b5b7c
Valeev Serj <145398532+Gabryelf@users.noreply.github.com>
2024-04-23 21:50:40 +0300

    Add files via upload

---
+++ background.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+    // Функция для загрузки изображений в блок header
+    function loadBackgroundImages() {
+        var imagesCount = 23; // Количество изображений в папке background
+        var currentIndex = 1; // Индекс текущего изображения
+
+        setInterval(function() {
+            // Формируем путь к изображению
+            var imagePath = 'background/back' + currentIndex + '.jpg';
+            // Загружаем изображение в блок backgroundImages
+            $('#backgroundImages').css('background-image', 'url(' + imagePath + ')');
+
+            // Увеличиваем индекс текущего изображения
+            currentIndex = (currentIndex % imagesCount) + 1;
+        }, 10000); // Загружаем каждое изображение на 10 секунд
+
+        setTimeout(function() {
+            // Затухание изображения перед сменой
+            $('#backgroundImages').fadeOut(5000, function() {
+                // Изменяем изображение с обратным эффектом затухания
+                setInterval(function() {
+                    // Формируем путь к изображению
+                    var imagePath = 'background/back' + currentIndex + '.jpg';
+                    // Загружаем изображение в блок backgroundImages
+                    $('#backgroundImages').css('background-image', 'url(' + imagePath + ')');
+
+                    // Уменьшаем индекс текущего изображения
+                    currentIndex = (currentIndex == 1) ? imagesCount : (currentIndex - 1);
+                }, 10000); // Загружаем каждое изображение на 10 секунд
+            });
+        }, 5000); // Затухание на 5 секунд перед сменой изображения
+    }
+
+    loadBackgroundImages();
+});
